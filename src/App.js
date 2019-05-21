b2d79c29586164df9f780a9e78b065dcbf61af36
remifestou <remi.festou@icloud.com>
2019-05-21 23:16:13 +0200

    config app sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './assets/stylesheets/main.scss'
 
 function App() {
   return (
-    <section></section>
+    <section className="container">
+      <h1>App web de</h1>
+      <span className="icon icon-festou"></span>
+    </section>
   );
 }
 
