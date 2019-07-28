90faf43ff8d9c969e3c01acdade1e995c3cc6dc2
remifestou <remi.festou@icloud.com>
2019-07-28 18:43:33 +0200

    clean project and add contact page

--- src/App.js
+++ src/App.js
@@ -2,34 +2,20 @@ import React from 'react'
 import './tailwind/index.css';
 import './assets/stylesheets/main.scss'
 
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
-import Header from './components/Header.js'
-import Illustration from './components/Illustration.js'
-// import SocialNetwork from './components/SocialNetwork.js'
-// import Skills from './containers/Skills.js'
-
-// AOS.init({
-//   disable: "mobile"
-// });
+import Header from './components/Header'
+import Index from './pages/Index'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <section className="w-full max-w-6xl pt-16 mx-auto h-screen relative">
-      <Header />
-      <Illustration />
-      {/* <Logo />
-      <div className="md:mb-2">
-        <Skills />
-      </div>
-      <div 
-        className="flex justify-center items-center mb-4 md:mt-8"
-        data-aos="fade-up"
-        data-aos-delay="1600"
-      >
-        <SocialNetwork />
-      </div> */}
+      <Router>
+        <Header />
+        <Route exact path="/" component={Index} />
+        <Route path="/contact" component={Contact} />
+      </Router>
     </section>
   );
 }
