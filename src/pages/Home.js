69f1288b41973a6399f9fa1c0d2829372e01d107
remifestou <remi.festou@icloud.com>
2019-08-21 10:17:50 +0200

    remove test form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import React from 'react'
 import Illustration from './../components/illustrations/Home'
 import Title from '../components/Title'
 import Keywords from '../components/Keywords'
-import ContactForm from './../components/forms/Contact'
 
 function Home() {
   return (
@@ -11,7 +10,6 @@ function Home() {
         <Title />
         <Keywords />
         <Illustration />
-        <ContactForm />
     </div>
   );
 }
